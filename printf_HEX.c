b7fb727f501b62e0e924e6ad8d44a6404e2c359d
Mirgubad <154666707+mirgubadakbarov@users.noreply.github.com>
2024-03-28 06:17:08 +0400

    Update printf_HEX.c

--- printf_HEX.c
+++ printf_HEX.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * printf_hex - prints an hexgecimal number.
+ * printf_HEX - prints an hexgecimal number.
  * @val: arguments.
  * Return: counter.
  */
-int printf_hex(va_list val)
+int printf_HEX(va_list val)
 {
 	int i;
 	int *array;
